a5de20b33dbda2dc4910fd0567072fb5d949caf6
John Keyes <john@keyes.ie>
2015-03-26 22:54:35 +0000

    Adding a httpretty test.

--- test_nose.py
+++ test_nose.py
@@ -1,3 +1,15 @@
 
 def test_add():
     pass
+
+
+import httpretty
+import requests
+
+
+@httpretty.activate
+def test_get():
+    httpretty.register_uri(
+        httpretty.GET, 'http://example.com', body=None, status=200)
+    response = requests.get('http://example.com')
+    assert response.status_code == 200
